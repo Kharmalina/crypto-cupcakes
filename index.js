6241d28e8ea24492b0866d277f6a12a388cf9a88
Kharmalina Tong <kharmalinat7@gmail.com>
2023-02-09 16:42:03 -0500

    add authorization middleware for get and post route handlers

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = process.env;
 // TODO - require express-openid-connect and destructure auth from it
 const { auth } = require('express-openid-connect');
 const { User, Cupcake } = require('./db');
@@ -29,7 +31,7 @@ app.use(express.urlencoded({extended:true}));
   } = process.env;
   
   const config = {
-    authRequired: true, // this is different from the documentation
+    authRequired: false, // this is different from the documentation
     auth0Logout: true,
     secret: AUTH0_SECRET,
     baseURL: AUTH0_AUDIENCE,
@@ -40,9 +42,41 @@ app.use(express.urlencoded({extended:true}));
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
+
+// auth middleware to use JWT and set the returned data as req.user
+const setUser = async (req, res, next) => {
+  const auth = req.header("Authorization");
+  console.log(auth)
+  if(!auth){
+    console.log("auth does not exist")
+      next();
+  } else {
+      const [, token] = auth.split(' ');
+      const user = jwt.verify(token, JWT_SECRET);
+      console.log("This is my token: ", token)
+      req.user = user;
+      next();
+  }
+}
+
+// middleware to find or create a user and save the user data in the db
+app.use(async (req, res, next) => {
+  console.log("findorCreate")
+  console.log("req.oidc:", req.oidc)
+  const [user] = await User.findOrCreate({
+    where: {
+      username: req.oidc.user.nickname,
+      name: req.oidc.user.name,
+      email: req.oidc.user.email
+    }
+  });
+  console.log("user: ", user);
+  next();
+});
+
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  console.log(req.oidc.user);
+app.get('/', setUser, (req, res) => {
+  console.log("req.oidc.user: ", req.oidc.user);
   res.send(req.oidc.isAuthenticated() ? 
   `<h1>My Web App, Inc.</h1> 
   <h2>Welcome, ${req.oidc.user.name}</h2> 
@@ -55,10 +89,46 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/cupcakes', async (req, res, next) => {
+app.get('/cupcakes', setUser, async (req, res, next) => {
   try {
     const cupcakes = await Cupcake.findAll();
     res.send(cupcakes);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// GET /me sends back user data and token
+app.get('/me', setUser, async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.oidc.user.nickname
+      }
+      ,
+      raw: true
+  });
+  if (user) {
+    const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1w' });
+    res.send({user, token})
+  }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.post('/cupcakes', setUser, async (req, res, next) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      res.status(401).send("Request Denied: expect valid token")
+    } else {
+      const {title, flavor, stars} = req.body;
+      const createdCupcake = await Cupcake.create({title, flavor, stars, userId: req.user.id});
+      res.send(createdCupcake);
+    }
   } catch (error) {
     console.error(error);
     next(error);
